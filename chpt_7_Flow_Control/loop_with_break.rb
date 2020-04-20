04d451fb9448262fbbc222e71fba04fd00e21b19
joselondon <copovikingj@gmail.com>
2020-04-20 17:46:52 +0100

    Adds two looping examples one basic, one with a break statement

---
+++ chpt_7_Flow_Control/loop_with_break.rb
@@ -0,0 +1,11 @@
+# loop_with_break.rb
+
+while true
+  input = gets.chomp
+  puts input
+  if input == 'bye'
+    break
+  end
+end
+
+puts 'Please come again soon!'
---
+++ chpt_7_Flow_Control/looping_example.rb
@@ -0,0 +1,8 @@
+# looping_example.rb
+
+input = ''
+while input != 'bye'
+  puts input
+  input = gets.chomp
+end
+puts 'Come again soon!'
