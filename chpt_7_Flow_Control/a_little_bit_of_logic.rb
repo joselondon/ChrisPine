e5d1fe2c71aed6ea8b3ab425c2f2c2d0bff02228
joselondon <copovikingj@gmail.com>
2020-04-25 12:32:55 +0100

    Refactor 2 removes duplicate ifelse logic replaces with OR conditional to make DRYier

--- chpt_7_Flow_Control/a_little_bit_of_logic.rb
+++ chpt_7_Flow_Control/a_little_bit_of_logic.rb
@@ -4,8 +4,6 @@ puts 'Hello, what\'s your name?'
 name = gets.chomp
 puts  'Hello, ' + name + '.'
 
-if name == 'Chris'
+if name == 'Chris' || name == 'Katy'
   puts 'What a lovely name!'
-elsif name == 'Katy'
-    puts 'What a lovely name!'
 end
