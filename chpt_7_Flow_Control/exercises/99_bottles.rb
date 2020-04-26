f21c9c5f65240fe16db934b57714caa2ace2578f
joselondon <copovikingj@gmail.com>
2020-04-26 12:31:41 +0100

    extracts number of bottles into a variable

--- chpt_7_Flow_Control/exercises/99_bottles.rb
+++ chpt_7_Flow_Control/exercises/99_bottles.rb
@@ -1,7 +1,9 @@
 # 99_bottles.rb
 
-puts "99 bottles of beer on the wall, 99 bottles of beer.
-Take one down, pass it around, 98 bottles of beer on the wall..."
-puts 
+number_of_bottles = 99
+
+puts "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
+Take one down, pass it around, #{number_of_bottles - 1} bottles of beer on the wall..."
+puts
 puts "No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall..."
