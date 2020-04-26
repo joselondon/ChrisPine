65e87354b5a044a820ced129c18bd382ba933ba6
joselondon <copovikingj@gmail.com>
2020-04-26 13:01:32 +0100

    Refactors number of bottles reduction to make shorter

--- chpt_7_Flow_Control/exercises/99_bottles.rb
+++ chpt_7_Flow_Control/exercises/99_bottles.rb
@@ -3,10 +3,10 @@
 number_of_bottles = 99
 
 while number_of_bottles > 1
-puts "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
-Take one down, pass it around, #{number_of_bottles - 1} bottles of beer on the wall..."
-puts
+puts "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer."
 number_of_bottles -= 1
+puts "Take one down, pass it around, #{number_of_bottles} bottles of beer on the wall..."
+puts
 end
 puts
 puts "No more bottles of beer on the wall, no more bottles of beer.
