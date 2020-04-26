29cd626f31eebe677dd32a1b056276c7e8e80b3c
joselondon <copovikingj@gmail.com>
2020-04-26 12:40:45 +0100

    Introduces first pass of working reduction of bottles, using while clause to reduce to 1 then print closing verse to screen.  Its a little messy and I do not like the '1 bottles...' in penultimate verse

--- chpt_7_Flow_Control/exercises/99_bottles.rb
+++ chpt_7_Flow_Control/exercises/99_bottles.rb
@@ -2,8 +2,12 @@
 
 number_of_bottles = 99
 
+while number_of_bottles > 1
 puts "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
 Take one down, pass it around, #{number_of_bottles - 1} bottles of beer on the wall..."
+puts
+number_of_bottles -= 1
+end
 puts
 puts "No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall..."
