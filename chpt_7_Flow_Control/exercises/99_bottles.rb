cb06bf0193b5b2e7d79c9d390b7a65c158b4965d
joselondon <copovikingj@gmail.com>
2020-04-26 12:28:06 +0100

    Beginnig and end of song

---
+++ chpt_7_Flow_Control/exercises/99_bottles.rb
@@ -0,0 +1,7 @@
+# 99_bottles.rb
+
+puts "99 bottles of beer on the wall, 99 bottles of beer.
+Take one down, pass it around, 98 bottles of beer on the wall..."
+puts 
+puts "No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall..."
