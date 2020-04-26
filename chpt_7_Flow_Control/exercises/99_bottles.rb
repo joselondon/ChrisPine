291a1dac1b35c2dad534729848d51af2c7c6112c
joselondon <copovikingj@gmail.com>
2020-04-26 13:13:11 +0100

    Refactors while clasue to run while true, introduces break clause at 2, allowing for singular grammer in penultimate verse.  Completes exercise

--- chpt_7_Flow_Control/exercises/99_bottles.rb
+++ chpt_7_Flow_Control/exercises/99_bottles.rb
@@ -2,12 +2,18 @@
 
 number_of_bottles = 99
 
-while number_of_bottles > 1
-puts "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer."
-number_of_bottles -= 1
-puts "Take one down, pass it around, #{number_of_bottles} bottles of beer on the wall..."
-puts
+while true
+  puts "#{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer."
+  number_of_bottles -= 1
+    if number_of_bottles < 2
+      break
+    end
+  puts "Take one down, pass it around, #{number_of_bottles} bottles of beer on the wall..."
+  puts
 end
+
+puts "Take one down, pass it around, 1 more bottle of beer on the wall..."
 puts
+
 puts "No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall..."
