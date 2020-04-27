3e11f1a620b5add63ebd948d8d364326b85efd53
joselondon <copovikingj@gmail.com>
2020-04-27 17:39:52 +0100

    Add break conditional of 'BYE!' and randomises year grandma shouts to a range between 1939 and 1951

--- chpt_7_Flow_Control/exercises/deaf_grandma.rb
+++ chpt_7_Flow_Control/exercises/deaf_grandma.rb
@@ -3,10 +3,10 @@ puts "Say something to grandma"
 
 while true
   input = gets.chomp
-  if input == input.upcase
+  if input == 'BYE!'
     break
   end
   puts "HUH?! SPEAK UP, SONNY!"
 end
 
-puts "NO, NOT SINCE 1938!"
+puts "NO, NOT SINCE #{rand(1930..1951)}!"
