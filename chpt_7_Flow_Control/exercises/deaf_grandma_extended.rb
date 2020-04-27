01ee3819a34b328249b479477068a9771167bb89
joselondon <copovikingj@gmail.com>
2020-04-27 21:18:38 +0100

    Adds first pass working extended_deaf_grandma.rb  however its a little messy in that she repeat 'Speak up sonny' before the break phrase'

---
+++ chpt_7_Flow_Control/exercises/deaf_grandma_extended.rb
@@ -0,0 +1,17 @@
+# deaf_grandma_extended.rb
+require 'pry'
+puts "Say something to grandma"
+bye_counter = 0
+
+while bye_counter < 3
+  input = gets.chomp
+  if input == 'BYE!'
+    bye_counter += 1
+  else
+    bye_counter = 0
+  end
+  puts "HUH?! SPEAK UP, SONNY!"
+end
+
+puts "NO, NOT SINCE #{rand(1930..1951)}!"
+#  I think the author was looking for year =rand(21) + 20 I prefer my solution!
