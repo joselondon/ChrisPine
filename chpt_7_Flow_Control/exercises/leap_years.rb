b782681a4eed9274d7cd1ca11323c872b3b6e589
joselondon <copovikingj@gmail.com>
2020-04-28 18:59:18 +0100

    Set up prompts for start and end years in the range

---
+++ chpt_7_Flow_Control/exercises/leap_years.rb
@@ -0,0 +1,9 @@
+# leap_years.rb
+
+puts "Welcome to the Leap Years calculator"
+puts "Please enter your start year"
+
+start_year = gets.chomp
+
+puts "Now please enter your end year"
+end_year = gets.chomp
