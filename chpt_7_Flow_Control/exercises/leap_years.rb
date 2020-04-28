efe8450e7e84c9a36c9b6bf1a7dc7b8ccb602806
joselondon <copovikingj@gmail.com>
2020-04-28 20:25:45 +0100

    Prints out all of the years between start and end dates

--- chpt_7_Flow_Control/exercises/leap_years.rb
+++ chpt_7_Flow_Control/exercises/leap_years.rb
@@ -3,7 +3,14 @@
 puts "Welcome to the Leap Years calculator"
 puts "Please enter your start year"
 
-start_year = gets.chomp
+start_year = gets.chomp.to_i
 
 puts "Now please enter your end year"
-end_year = gets.chomp
+end_year = gets.chomp.to_i
+
+year_count = end_year
+
+while year_count >= start_year
+  puts year_count
+  year_count -= 1
+end
