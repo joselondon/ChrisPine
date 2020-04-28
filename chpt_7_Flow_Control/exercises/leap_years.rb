56ed76f71fb980c8af644fe1c546a9a80e794abc
joselondon <copovikingj@gmail.com>
2020-04-28 20:28:30 +0100

    Adds all the basic leap years, ie just the ones that divide by four evenly

--- chpt_7_Flow_Control/exercises/leap_years.rb
+++ chpt_7_Flow_Control/exercises/leap_years.rb
@@ -8,9 +8,13 @@ start_year = gets.chomp.to_i
 puts "Now please enter your end year"
 end_year = gets.chomp.to_i
 
+puts
+
 year_count = end_year
 
 while year_count >= start_year
-  puts year_count
-  year_count -= 1
+  if year_count % 4 == 0
+    puts year_count
+  end
+    year_count -= 1
 end
