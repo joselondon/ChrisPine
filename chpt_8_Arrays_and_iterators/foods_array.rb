cb9047bb0d4bdd30daa5c9c839c56839bc189b07
joselondon <copovikingj@gmail.com>
2020-05-05 14:04:28 +0100

    foods_array and go-go-gadget-integer-method iterator examples from book

---
+++ chpt_8_Arrays_and_iterators/foods_array.rb
@@ -0,0 +1,15 @@
+# foods_array.rb
+
+foods = ['artichoke','brioche','caramel']
+
+puts foods
+puts
+puts foods.to_s
+puts
+puts foods.join(', ')
+puts
+puts foods.join(' :) ') + ' 8)'
+
+200.times do
+  puts [[1,2,['a','b'],3]]
+end
