e342f2d29be75b2eadb093df985d87fcace25e7a
joselondon <copovikingj@gmail.com>
2020-05-30 15:17:45 +0100

    Adds solution for 'old school roman numeral generator ' exercise

--- chpt_9_writing_your_own_methods/exercises/old_school_roman_numerals.rb
+++ chpt_9_writing_your_own_methods/exercises/old_school_roman_numerals.rb
@@ -1,15 +1,38 @@
 # old_school_roman_numerals.rb
 
-puts "Welome to the old school roman numeral converter."
-puts "Please enter an integer from 0 to 3000..."
+def old_roman_num_converter(n)
+  if n > 3000 || n < 1
+    puts "Please enter a number between 1 and 3000"
+    int = gets.chomp.to_i
+  else
 
-int = gets.chomp.to_i
-old_roman_num = ""
+    int = n.to_i
+    original_num = int
+    old_roman_num = []
+  end
 
-if int/1000 > 0
-  old_roman_num = 'M' * (int/1000)
-else
-  puts "meh"
+
+  while int > 0
+    old_roman_num = 'M' * (int/1000)
+    int = int % 1000
+    old_roman_num << ('D' * (int/500))
+    int = int % 500
+    old_roman_num << ('C' * (int/100))
+    int = int % 100
+    old_roman_num << ('L' * (int/50))
+    int = int % 50
+    old_roman_num << ('X' * (int/10))
+    int = int % 5
+    old_roman_num << ('V' * (int/5))
+    int = int % 5
+    old_roman_num << 'I' * int
+    int = 0
+  end
+  p  old_roman_num
 end
 
-puts "#{int} as an old school roman numberal is:  #{old_roman_num}"
+
+old_roman_num_converter(2351)
+puts old_roman_num_converter(0)
+old_roman_num_converter(-12)
+old_roman_num_converter(4000)
