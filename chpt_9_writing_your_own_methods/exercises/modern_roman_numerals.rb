695781467f0a8b80f09d76a8739d9199db269df0
joselondon <copovikingj@gmail.com>
2020-06-11 18:37:22 +0100

    Adds modern_roman_numeral. - only works for multiples of 1000, 5500 and 100

--- chpt_9_writing_your_own_methods/exercises/modern_roman_numerals.rb
+++ chpt_9_writing_your_own_methods/exercises/modern_roman_numerals.rb
@@ -1,26 +1,25 @@
 # modern_roman_numerals.rb
+# book solution
 
-def modern_roman_numeral_generator(n)
-  if n > 3000 || n < 1
-    puts "Please enter a number between 1 and 3000"
-    int = gets.chomp.to_i
+def roman_numeral(num)
+  thous = num/1000
+  hunds = (num % 1000 / 100)
+  tens = (num % 100 / 10)
+  ones = (num % 10)
+
+  roman = 'M' * thous
+puts hunds
+  if hunds == 9
+    roman << 'CM'
+  elsif hunds == 4
+    roman << 'CD'
+  elsif hunds == 5
+    roman << 'D'
   else
-    int = n.to_i
-    new_roman_num = []
+    roman = roman + 'D' * (num % 1000 / 500)
+    roman = roman + 'C' * (num % 500 / 100)
   end
 
-  while int > 0
-    new_roman_num = 'M' * (int/1000)
-    int = int % 1000
-    if int % 500 == 400
-      new_roman_num << 'C'
-    end
-    new_roman_num << 'D' * (int/500)
-    int = int % 500
-    # line 21 just a hold to remove the excess 100s until program is completed
-    int = int - 400
-  end
-  puts new_roman_num
 end
 
-puts modern_roman_numeral_generator(1900)
+puts roman_numeral(3400)
