ec98de20e61a07e7f04a263f00887e1c00151d2f
joselondon <copovikingj@gmail.com>
2020-06-15 13:42:19 +0100

    Adds working modern roman numeral generator worked through from book

--- chpt_9_writing_your_own_methods/exercises/modern_roman_numerals.rb
+++ chpt_9_writing_your_own_methods/exercises/modern_roman_numerals.rb
@@ -8,7 +8,6 @@ def roman_numeral(num)
   ones = (num % 10)
 
   roman = 'M' * thous
-puts hunds
   if hunds == 9
     roman << 'CM'
   elsif hunds == 4
@@ -20,6 +19,23 @@ puts hunds
     roman = roman + 'C' * (num % 500 / 100)
   end
 
+  if tens == 9
+    roman + 'XC'
+  elsif tens == 4
+    roman + 'XL'
+  else
+    roman + 'L' * (num % 100 / 50)
+    roman + 'X' * (num % 50 / 10)
+  end
+
+  if ones == 9
+    roman + 'IX'
+  elsif ones == 4
+    roman + 'IV'
+  else
+    roman + 'V' * (num % 10 /5 )
+    roman + 'I' * (num % 5 / 1)
+  end
 end
 
-puts roman_numeral(3400)
+puts roman_numeral(3699)
