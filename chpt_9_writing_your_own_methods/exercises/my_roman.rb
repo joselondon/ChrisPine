9d1304f5a12624f08d951314ef9777257173400f
joselondon <copovikingj@gmail.com>
2020-07-10 11:10:36 +0100

    One working self corrected roman numeral gerator for numbers between 1 and 999999

--- chpt_9_writing_your_own_methods/exercises/my_roman.rb
+++ chpt_9_writing_your_own_methods/exercises/my_roman.rb
@@ -32,11 +32,47 @@ roman = 'M' * thous
     roman = roman << 'IV'
   else
     roman = roman << 'V' * (n % 10 /5)
-    roman = roman << 'I' * ones
+    roman = roman << 'I' * (n % 5)
   end
-
 
 puts roman
 end
 
-modern_roman(2935)
+modern_roman(1)
+modern_roman(2)
+modern_roman(4)
+modern_roman(5)
+modern_roman(8)
+modern_roman(9)
+modern_roman(10)
+modern_roman(14)
+modern_roman(18)
+modern_roman(19)
+modern_roman(20)
+modern_roman(40)
+modern_roman(50)
+modern_roman(90)
+modern_roman(100)
+modern_roman(219)
+modern_roman(249)
+modern_roman(250)
+modern_roman(280)
+modern_roman(290)
+modern_roman(490)
+modern_roman(500)
+modern_roman(991)
+modern_roman(994)
+modern_roman(998)
+modern_roman(999)
+modern_roman(1000)
+modern_roman(1001)
+modern_roman(1005)
+modern_roman(1004)
+modern_roman(1008)
+modern_roman(1009)
+modern_roman(1010)
+modern_roman(1049)
+modern_roman(1050)
+modern_roman(1999)
+modern_roman(2000)
+modern_roman(999999)
