0ac57659ce877de92dc3b4d14d31c4a14d17734a
joselondon <copovikingj@gmail.com>
2020-07-17 18:03:56 +0100

    Adds the world mapr for the civ_continent_sizer.rb

---
+++ chpt_10_recursion/civ_continent_sizer.rb
@@ -0,0 +1,18 @@
+# civ_continent_sizer.rb
+
+M = 'Land'
+o = 'Water'
+
+world = [
+          [o,o,o,o,o,o,o,o,o,o,o],
+          [o,o,o,o,M,M,o,o,o,o,o],
+          [o,o,o,o,o,o,o,o,M,M,o],
+          [o,o,o,M,o,o,o,o,o,M,o],
+          [o,o,o,M,o,M,M,o,o,o,o],
+          [o,o,o,o,M,M,M,M,o,o,o],
+          [o,o,o,M,M,M,M,M,M,M,o],
+          [o,o,o,M,M,o,M,M,M,o,o],
+          [o,o,o,o,o,o,M,M,o,o,o],
+          [o,M,o,o,o,M,o,o,o,o,o],
+          [o,o,o,o,o,o,o,o,o,o,o]
+        ]
