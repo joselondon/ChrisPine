b0169eea41533c9790a3dfc106676d76ef1b73e6
joselondon <copovikingj@gmail.com>
2020-07-18 18:24:17 +0100

    Worked example from book pg 73, (civ_continent_sizer.rb).  To understand real world example of recursion use

--- chpt_10_recursion/civ_continent_sizer.rb
+++ chpt_10_recursion/civ_continent_sizer.rb
@@ -1,7 +1,7 @@
 # civ_continent_sizer.rb
 
-M = 'Land'
-o = 'Water'
+M = 'land'
+o = 'water'
 
 world = [
           [o,o,o,o,o,o,o,o,o,o,o],
@@ -16,3 +16,27 @@ world = [
           [o,M,o,o,o,M,o,o,o,o,o],
           [o,o,o,o,o,o,o,o,o,o,o]
         ]
+
+def continent_size (world, x, y)
+  if world[y][x] != 'land'
+    # Either it is water or we already counted it,
+    # but either way, we do not want to count it now.
+    return 0
+  end
+  # So first we count this tile..
+  size = 1
+  world[y][x] = 'counted land'
+#  ...then we count all of the neighbouring eight tiles
+# (and of course their nighbours by way of the recursion).
+  size = size + continent_size(world, x-1, y-1)
+  size = size + continent_size(world, x  , y-1)
+  size = size + continent_size(world, x+1, y-1)
+  size = size + continent_size(world, x-1, y  )
+  size = size + continent_size(world, x+1, y  )
+  size = size + continent_size(world, x-1, y+1)
+  size = size + continent_size(world, x  , y+1)
+  size = size + continent_size(world, x+1, y+1)
+  size
+end
+
+puts continent_size(world, 5, 5)
