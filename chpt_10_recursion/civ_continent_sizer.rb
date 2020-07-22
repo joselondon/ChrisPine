2fcdc81aa53a0a2d4ec8882dd64b6cab3e073b5b
joselondon <copovikingj@gmail.com>
2020-07-22 19:19:08 +0100

    I think there is an error in author's code preventing the code calculating the smaller island sizes.  In continent_size(world x, y) the x and y array elements were transposed within the menthod.  If I correct this then the method works fine calculating smaller islands.

--- chpt_10_recursion/civ_continent_sizer.rb
+++ chpt_10_recursion/civ_continent_sizer.rb
@@ -3,29 +3,28 @@
 M = 'land'
 o = 'water'
 
-world = [
-          [o,o,o,o,o,o,o,o,o,o,o],
-          [o,o,o,o,M,M,o,o,o,o,o],
-          [o,o,o,o,o,o,o,o,M,M,o],
-          [o,o,o,M,o,o,o,o,o,M,o],
-          [o,o,o,M,o,M,M,o,o,o,o],
-          [o,o,o,o,M,M,M,M,o,o,o],
-          [o,o,o,M,M,M,M,M,M,M,o],
-          [o,o,o,M,M,o,M,M,M,o,o],
-          [o,o,o,o,o,o,M,M,o,o,o],
-          [o,M,o,o,o,M,o,o,o,o,o],
-          [o,o,o,o,o,o,o,o,o,o,o]
-        ]
+world =
+        [[o,o,o,o,o,o,o,o,o,o,o],
+         [o,o,o,o,M,M,o,o,o,o,o],
+         [o,o,o,o,o,o,o,o,M,M,o],
+         [o,o,o,M,o,o,o,o,o,M,o],
+         [o,o,o,M,o,M,M,o,o,o,o],
+         [o,o,o,o,M,M,M,M,o,o,o],
+         [o,o,o,M,M,M,M,M,M,M,o],
+         [o,o,o,M,M,o,M,M,M,o,o],
+         [o,o,o,o,o,o,M,M,o,o,o],
+         [o,M,o,o,o,M,o,o,o,o,o],
+         [o,o,o,o,o,o,o,o,o,o,o]]
 
 def continent_size (world, x, y)
-  if world[y][x] != 'land'
+  if world[x][y] != 'land'
     # Either it is water or we already counted it,
     # but either way, we do not want to count it now.
     return 0
   end
   # So first we count this tile..
   size = 1
-  world[y][x] = 'counted land'
+  world[x][y] = 'counted land'
 #  ...then we count all of the neighbouring eight tiles
 # (and of course their nighbours by way of the recursion).
   size = size + continent_size(world, x-1, y-1)
@@ -39,4 +38,6 @@ def continent_size (world, x, y)
   size
 end
 
+
+puts continent_size(world, 1, 4)
 puts continent_size(world, 5, 5)
