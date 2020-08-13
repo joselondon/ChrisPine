f90e90c6f23a31de46658aad93b3d9a7f8677c2f
joselondon <copovikingj@gmail.com>
2020-08-13 10:51:32 +0100

    Correctly transposed xy, to take account of cartesion x,y format against array format

--- chpt_10_recursion/civ_continent_sizer_part2.rb
+++ chpt_10_recursion/civ_continent_sizer_part2.rb
@@ -1,59 +1,43 @@
-# civ_continent_sizer.rb
+# civ_continent_sizer_part2.rb
 require 'pry'
 
 M = 'land'
 o = 'water'
 # size  = 1
 world =
-        [[o,o,o,o,o,o,o,o,o,o,o],
+        [[o,o,o,o,o,M,o,o,o,o,o],
          [o,o,o,o,M,M,o,o,o,o,o],
-         [o,o,o,o,o,o,o,o,M,M,o],
-         [o,o,o,M,o,o,o,o,o,M,o],
+         [o,o,o,o,o,M,o,o,M,M,o],
+         [o,o,o,M,o,M,o,o,o,M,o],
          [o,o,o,M,o,M,M,o,o,o,o],
          [o,o,o,o,M,M,M,M,o,o,o],
-         [o,o,o,M,M,M,M,M,M,M,o],
+         [M,M,M,M,M,M,M,M,M,M,M],
          [o,o,o,M,M,o,M,M,M,o,o],
          [o,o,o,o,o,o,M,M,o,o,o],
          [o,M,o,o,o,M,o,o,o,o,o],
-         [o,o,o,o,o,o,o,o,o,o,o]]
+         [o,o,o,o,o,M,o,o,o,o,o]]
 
 def continent_size (world, x, y)
-  if world[x][y] != 'land'
+  if world[y][x] != 'land'
     # Either it is water or we already counted it,
     # but either way, we do not want to count it now.
     return 0
   end
   # So first we count this tile..
   size = 1
-  world[x][y] = 'counted land'
+  world[y][x] = 'counted land'
 #  ...then we count all of the neighbouring eight tiles
 # (and of course their nighbours by way of the recursion).
   size = size + continent_size(world, x-1, y-1)
-  #M = 5, y = 5 becasue M-1,y-1 returns 0 and breaks
   size = size + continent_size(world, x  , y-1)
-  #M = 4, y = 3
-  size = size + continent_size(world, x+1, y-1) # 5,2 = w therefore passing on 4, 3 to neMt
-  #M=4, y=3
-  size = size + continent_size(world, x-1, y  ) #3,3 = l ; 2,3 = w
-  # M = 3, y = 3
-  size = size + continent_size(world, x+1, y  ) # 4, 3=c
-  # M=3, y=3
+  size = size + continent_size(world, x+1, y-1)
+  size = size + continent_size(world, x-1, y  )
+  size = size + continent_size(world, x+1, y  )
   size = size + continent_size(world, x-1, y+1)
   size = size + continent_size(world, x  , y+1)
   size = size + continent_size(world, x+1, y+1)
   size
 end
-=begin
-ME STEPPING THROUGH THIS CODE TO UNDESTAND!!!!
-size = 1
-base case (5,5)-L
-(4,4)-W return 0
 
-=end
-
-# puts continent_size(world, 1, 4)
 puts continent_size(world, 5, 5)
-
-def args(a)
-
-end
+puts continent_size(world, 11,11)
