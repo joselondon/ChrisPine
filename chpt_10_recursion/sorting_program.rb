0e5cf085b5cc128bc88efbf584a849462a159d6a
joselondon <copovikingj@gmail.com>
2020-08-27 21:40:49 +0100

    Adds variable to poin to string input for words

--- chpt_10_recursion/sorting_program.rb
+++ chpt_10_recursion/sorting_program.rb
@@ -1,5 +1,13 @@
 #sorting_program.rb
 
+list = []
+
 puts "Welcome to the word sorting program. Please enter your words one at "
 puts "a time each on a separate line.  When you are done just hit <ENTER>"
 puts "on an empty line.  The program will then sort your words."
+
+word = gets.chomp
+
+list << word
+
+puts list
