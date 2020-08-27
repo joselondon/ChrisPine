8f62ef5898b79215387023e44459afb68dd717c5
joselondon <copovikingj@gmail.com>
2020-08-27 19:50:58 +0100

    Welcome message and instructions

---
+++ chpt_10_recursion/sorting_program.rb
@@ -0,0 +1,5 @@
+#sorting_program.rb
+
+puts "Welcome to the word sorting program. Please enter your words one at "
+puts "a time each on a separate line.  When you are done just hit <ENTER>"
+puts "on an empty line.  The program will then sort your words."
