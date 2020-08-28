fb6172483c4955ac911bbba41142c69c32f910f6
joselondon <copovikingj@gmail.com>
2020-08-28 20:00:34 +0100

    refactors again to include instance variables that can be passed between variabls for use when I start to sort lists between methods

--- chpt_10_recursion/sorting_program.rb
+++ chpt_10_recursion/sorting_program.rb
@@ -22,12 +22,23 @@ puts "Welcome to the word sorting program. Please enter your words one at "
 puts "a time each on a separate line.  When you are done just hit <ENTER>"
 puts "on an empty line.  The program will then sort your words."
 
-list = []
-word = gets.chomp
 
-while word != ""
-  list << word
-  word = gets.chomp
+
+def get_word
+@list = []
+@word = gets.chomp
+
+  while @word != ""
+    @list << @word
+    @word = gets.chomp
+  end
 end
 
-puts list
+get_word
+
+def sort
+  @sorted_list = []
+  @unsorted_list = @list
+
+  # unsorted_list[0] 
+end
