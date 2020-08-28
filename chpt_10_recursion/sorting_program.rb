8213a234565cf16f36f332d0012aad2537d3d2e1
joselondon <copovikingj@gmail.com>
2020-08-28 18:56:07 +0100

    Refactors my crazy grab an array of inputted words method

--- chpt_10_recursion/sorting_program.rb
+++ chpt_10_recursion/sorting_program.rb
@@ -16,18 +16,18 @@ puts list
 
 #########################################################
 
-def get_word
-  list = []
-  word = gets.chomp
-  if word != ""
-    list << word
-    get_word
-  end
-  puts list
-end
+
 
 puts "Welcome to the word sorting program. Please enter your words one at "
 puts "a time each on a separate line.  When you are done just hit <ENTER>"
 puts "on an empty line.  The program will then sort your words."
 
-word = get_word
+list = []
+word = gets.chomp
+
+while word != ""
+  list << word
+  word = gets.chomp
+end
+
+puts list
