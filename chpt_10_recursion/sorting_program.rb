952acdc7eb67c111ecf1478d41cdfb6897ddb871
joselondon <copovikingj@gmail.com>
2020-08-28 18:28:41 +0100

    Prompts for words to be added on separate lines and on hitting enter w/o entering a string - exits recursion and outputs array as unstorted string list

--- chpt_10_recursion/sorting_program.rb
+++ chpt_10_recursion/sorting_program.rb
@@ -1,5 +1,6 @@
 #sorting_program.rb
 
+=begin
 list = []
 
 puts "Welcome to the word sorting program. Please enter your words one at "
@@ -11,3 +12,22 @@ word = gets.chomp
 list << word
 
 puts list
+=end
+
+#########################################################
+
+def get_word
+  list = []
+  word = gets.chomp
+  if word != ""
+    list << word
+    get_word
+  end
+  puts list
+end
+
+puts "Welcome to the word sorting program. Please enter your words one at "
+puts "a time each on a separate line.  When you are done just hit <ENTER>"
+puts "on an empty line.  The program will then sort your words."
+
+word = get_word
